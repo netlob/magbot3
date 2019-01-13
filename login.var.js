aab12ea481564611c78ce02261ca92e47e281120
Sj3rd <33723199+Sj3rd@users.noreply.github.com>
2019-01-13 14:12:48 +0100

    Delete login.var.js

--- login.var.js
+++
@@ -1,8 +0,0 @@
-var login = [];
-login.username = 'sjoerd.bolten'
-login.password = 'Audiobox$123'
-login.school = 'Kaj Munk'
-login.calendarid = ''
-
-module.exports.magister = login;
-module.exports.calendar = login;
