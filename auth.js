e55d4c4243c8ee4587af344b6a07cf87be971796
Sj3rd <33723199+Sj3rd@users.noreply.github.com>
2019-01-13 12:44:46 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,9 +3,6 @@ const {google} = require('googleapis');
 const {google} = require('googleapis');
 
 const oauth2Client = new google.auth.OAuth2(
-    '312564690694-duurunfnut127m50dh0j1ajlhe9oq598.apps.googleusercontent.com',
-    'WJKnYCDWZHRNPdjH0PagYhx5',
-    'https://magbot.nl'
 );
 
 const scopes = [
