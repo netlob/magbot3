4ac838e5ea20871d8a2531d992d5e56ac1b51c8f
Sj3rd <sjoerdabolten@gmail.com>
2019-01-14 22:11:06 +0100

    I GOT IT WORKING

--- callback.webserver.js
+++ callback.webserver.js
@@ -1,4 +1,12 @@
 var http = require('http');
+const {google} = require('googleapis');
+
+var secret = require('./secret');
+const oauth2Client = new google.auth.OAuth2(
+  '312564690694-duurunfnut127m50dh0j1ajlhe9oq598.apps.googleusercontent.com',
+  secret.clientsecret,
+  'http://localhost:8080'
+);
 
 var params=function(req){
     let q=req.url.split('?'),result={};
@@ -17,4 +25,34 @@ var params=function(req){
 http.createServer(function(req,res){
     req.params=params(req);
     console.log(req.params.code);
+    poep(req.params.code)
 }).listen(8080);
+
+const poep = async function(code) {
+  const {tokens} = await oauth2Client.getToken(code)
+  oauth2Client.setCredentials(tokens);
+  listEvents(oauth2Client)
+}
+
+function listEvents(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  calendar.events.list({
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const events = res.data.items;
+    if (events.length) {
+      console.log('Upcoming 10 events:');
+      events.map((event, i) => {
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+      });
+    } else {
+      console.log('No upcoming events found.');
+    }
+  });
+}
