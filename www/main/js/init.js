9ef543acf0f68e2460d91c063d475b9c0da8cbca
Sj3rd <sjoerdabolten@gmail.com>
2019-01-14 20:06:27 +0100

    autocomplete school dropdown

--- www/main/js/init.js
+++ www/main/js/init.js
@@ -11,3 +11,28 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var schools = {};
+$("#autocomplete-input").on("input", function() {
+    if(this.value.length > 2){
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://mijn.magister.net/api/schools?filter=" + this.value,
+        "method": "GET",
+        "headers": {}
+      }
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        // var resvar = response
+        // for(var i = 0; i < resvar.length; i++){
+        //     // var school = '"'+resvar[i].name+'"'
+        //     var school = resvar[i].name
+        //     schools[school] = null
+        // }
+        // $('.autocomplete').autocomplete('updateData', schools);
+        // $('.autocomplete').autocomplete('open');
+      });
+    }
+});
