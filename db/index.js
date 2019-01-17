0641050e3bbc9689dfa9614de6b7cd97f8281d1b
Sj3rd <sjoerdabolten@gmail.com>
2019-01-17 14:55:59 +0100

    .

--- db/index.js
+++ db/index.js
@@ -1,8 +1,9 @@
 /*
- *   -= Magbot3 =-
- *  By Sj3rd & 7kasper
- * Licensed under MIT
- *   -= Magbot3 =-
- */
+*   -= Magbot3 =-
+*  By Sj3rd & 7kasper
+* Licensed under MIT
+*   -= Magbot3 =-
+*/
 
- // Helper class for database things.
+// Helper class for database things.
+const sqlite3 = require('sqlite3').verbose();
