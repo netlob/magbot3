18e07115e0bb41bb8cfe0a728f3b78b0d1672296
Sj3rd <sjoerdabolten@gmail.com>
2019-01-19 22:37:00 +0100

    Update init.js

--- docs/signup/js/init.js
+++ docs/signup/js/init.js
@@ -17,7 +17,7 @@ function autocompleteDropdown() {
 function signInCallback(authResult) {
   console.dir(authResult)
   if (authResult['code']) {
-    gapi.client.getBasicProfile()
+    var profile = gapi.getBasicProfile()
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
