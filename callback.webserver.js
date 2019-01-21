686b9f8ecdf9a1b2f87d63f26ce419d4b3eb0fc5
Sj3rd <sjoerdabolten@gmail.com>
2019-01-21 20:21:54 +0100

    Update callback.webserver.js

--- callback.webserver.js
+++ callback.webserver.js
@@ -95,8 +95,9 @@ function signup(tokens, params) {
 		fs.writeFile(path.toLowerCase(), encLogin, 'utf8', () => {
 			console.log('Login saved at: db/'+login.school+'-'+login.username+'-login.json');
 		});
-
-		console.dir(loginFunc(login))
+		var m = await loginFunc(login)
+		console.dir(m)
+		// console.dir(loginFunc(login))
 		// .then((all => {
 		// 	console.dir('Dit is m:' + all)
 		// }))
