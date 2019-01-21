08e6cab596663d2176318d1ee9708ddc3f5f93b5
Sj3rd <sjoerdabolten@gmail.com>
2019-01-21 12:21:34 +0100

    big updateeee

--- callback.webserver.js
+++ callback.webserver.js
@@ -9,6 +9,10 @@ var AES = require("crypto-js/aes");
 var SHA256 = require("crypto-js/sha256");
 var request = require("request");
 
+var loginFunc = require('./login.function');
+var delCalendar = require('./delCalendar.function')
+var pushCalendar = require('./pushCalendar.function');
+
 var key = require('./key')
 
 var secret = require('./secret')
@@ -94,148 +98,10 @@ function signup(tokens, params) {
 		fs.writeFile(path.toLowerCase(), encLogin, 'utf8', () => {
 			console.log('Login saved at: db/'+login.school+'-'+login.username+'-login.json');
 		});
-		loginFunc(login, tokens)
-		return 'succes'
+
+		loginFunc(login)
+		.then((poep => {
+			console.dir(poep)
+		}))
 	});
-}
-
-
-
-function loginFunc(login) {
-	oauth2Client.setCredentials(login.tokens);
-	var authcode = ''
-	https.get('https://raw.githubusercontent.com/simplyGits/magisterjs-authcode/master/code.json', (resp) => {
-		let data = '';
-		resp.on('data', (chunk) => {
-			data += chunk;
-		});
-		resp.on('end', () => {
-			authcode = data.replace('"','').replace('"','').replace(/(\r\n\t|\n|\r\t)/gm, "");
-			getSchools(login.school)
-			.then((schools) => schools[0])
-			.then((school) => magister({
-				school,
-				username: login.username,
-				password: login.password,
-			}))
-			.then((m) => {
-				m.appointments(day(-1), day(4))
-				.then((m => {
-					del(oauth2Client, login, m)
-				}))
-			}, (err) => {
-				console.error('something went wrong:', err);
-				});
-		});
-	}).on("error", (err) => {
-		console.log("Error: " + err.message);
-	});
-}
-
-function pushCalendar(auth, login, m) {
-	const calendar = google.calendar({version: 'v3', auth});
-	for(var i = 0; m.length - 1 >= i; i++){
-		if(m[i].isCancelled == (login.cancelled == 'true')){
-			var event = {
-				'summary': [m[i].classes[0]?toTitleCase(m[i].classes[0]):m[i].classes] + ' van ' + [m[i].teachers[0]?m[i].teachers[0].description:'niemand'],
-				'location': isNaN(m[i].location)?m[i].location:'lokaal '+m[i].location,
-				'description': m[i].annotation?m[i].annotation:m[i].description,
-				'start': {
-					'dateTime': m[i].start,
-					'timeZone': 'Europe/Amsterdam',
-				},
-				'end': {
-					'dateTime': m[i].end,
-					'timeZone': 'Europe/Amsterdam',
-				},
-				'reminders': {
-					'useDefault': false,
-					'overrides': [
-					  	{
-							'method': 'popup',
-							'minutes': Number(login.notify)
-						},
-					],
-				},
-			};
-
-			if(m[i].infoType > 1 && login.mail) {
-				event.reminders.overrides = [
-					{
-						'method': 'popup',
-						'minutes': Number(login.notify)
-					},
-					{
-						'method': 'email',
-						'minutes': 24 * 60 * 7
-					}
-				]
-			}
-
-			calendar.events.insert({
-					auth: auth,
-					calendarId: login.calendarid,
-					resource: event,
-				}, function(err, event) {
-				if (err) {
-					console.log('There was an error contacting the Calendar service: ' + err);
-					return;
-				}
-			})
-		}
-	}
-}
-
-function del(auth, login, m) {
-	const calendar = google.calendar({version: 'v3', auth});
-	calendar.events.list({
-		calendarId: login.calendarid,
-		singleEvents: true,
-		orderBy: 'startTime',
-	}, (err, res) => {
-		if (err) return console.log('The API returned an error: ' + err);
-		const events = res.data.items;
-		for(var i = 0; i < events.length; i++) {
-			var params = {
-				calendarId: login.calendarid,
-				eventId: eventId,
-			};
-			calendar.events.delete(params, function(err) {
-			if (err) {
-				console.log('The API returned an error: ' + err);
-				return;
-			}
-			});
-		}
-	});
-	pushCalendar(auth, login, m)
-}
-
-var params=function(req){
-	let q=req.url.split('?'),result={};
-	if(q.length>=2){
-		q[1].split('&').forEach((item)=>{
-			try {
-				result[item.split('=')[0]]=item.split('=')[1];
-			} catch (e) {
-				result[item.split('=')[0]]='';
-			}
-		})
-	}
-	return result;
-}
-
-function day(extra) {
-	if(!extra){extra = 0}
-	extra = extra + 1
-	return moment().businessAdd(extra)._d
-}
-
-function toTitleCase(str) {
-	return str.replace(
-		/\w\S*/g,
-		function(txt) {
-				return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-		}
-	);
 }
