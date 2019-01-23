289caa88d4474807ddba697e8cd8e025216a15ba
Sj3rd <sjoerdabolten@gmail.com>
2019-01-23 15:41:42 +0100

    .

--- authcode.exec.js
+++ authcode.exec.js
@@ -1,17 +1,17 @@
 const puppeteer = require('puppeteer');
 
 module.exports = async function () {
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
-        const page = await browser.newPage();
-        await page.goto('https://kajmunk.magister.net/');
+    const page = await browser.newPage();
+    await page.goto('https://kajmunk.magister.net/');
 
-        const request = await page.waitForRequest('https://accounts.magister.net/challenge/current');
-        const data = JSON.parse(request.postData());
+    const request = await page.waitForRequest('https://accounts.magister.net/challenge/current');
+    const data = JSON.parse(request.postData());
 
-        const newAuthCode = data.authCode;
+    const newAuthCode = data.authCode;
 
-        await browser.close();
-    
-        return newAuthCode;
+    await browser.close();
+
+    return newAuthCode;
 };
