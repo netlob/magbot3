09c692d6c612a376219c47df85a15d69e16c061c
7kasper <kasper77077@gmail.com>
2019-01-24 20:11:49 +0100

    Add startup for the webserver for testing.

--- magbot3.js
+++ magbot3.js
@@ -1,2 +1,7 @@
 // Dit is de root / index van het gebeuren.
 // Hier wordt de server gestart.
+
+// run webserver
+require('./lib/signup.webserver');
+
+//TODO: Actually do something in this script lol.
