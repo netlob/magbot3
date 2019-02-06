0be2035031f16291f8f5c6f36973001817df8f10
7kasper <kasper77077@gmail.com>
2019-02-06 22:34:03 +0100

    Testing etc. Make logging in work.

--- magbot3.js
+++ magbot3.js
@@ -2,6 +2,58 @@
 // Hier wordt de server gestart.
 
 // run webserver
-require('./lib/signup.webserver');
+//require('./lib/signup.webserver');
 
 //TODO: Actually do something in this script lol.
+
+
+/*
+ *   -= Magbot3 =-
+ *  Sj3rd & 7kasper
+ * Licensed under MIT
+ *   -= Magbot3 =-
+ */
+
+'use strict';
+
+// Imports
+const MagisterAuth = require('./lib/magister/authcode.function');
+const User = require('./lib/magbot/User');
+const {google} = require('googleapis');
+const secret = require('./secret');
+const oAuth = new google.auth.OAuth2(
+  '312564690694-duurunfnut127m50dh0j1ajlhe9oq598.apps.googleusercontent.com',
+  secret.clientsecret,
+  'https://beta.magbot.nl'
+);
+
+
+let sync = async function() {
+    // Get the current magister authcode before syncing users.
+    let mAuth = await MagisterAuth();
+
+    // Get all users from DB & run ofer them.
+    let users = await User.fetchAll();
+    for(let user of users) {
+        try {
+            if(user.get('username') == 'hammuller') {
+            // Login
+            await user.login(oAuth, mAuth);
+            console.dir(await user.appointments());
+            }
+        } catch(err) {
+            console.dir(err);
+        }
+    }
+    
+
+    // User.onEach(user => {
+    //     await user.login(oAuth, mAuth);
+
+    // });
+    
+
+
+}
+
+sync();
