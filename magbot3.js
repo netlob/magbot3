e1edcbdd4df56f567130174e6ffa115e35433bec
7kasper <kasper77077@gmail.com>
2019-02-25 23:54:11 +0100

    Add / fix purge.

--- magbot3.js
+++ magbot3.js
@@ -41,19 +41,23 @@ const oAuth = [
 ];
 const log = winston.loggers.get('main');
 
-run();
+const next = Math.floor(Math.random() * 10000);
+log.info(`Starting first sync in ${next} and first purge in ${next * 2} millis...`);
+setTimeout(sync, next);
+setTimeout(purge, next * 20);
 
 /**
  * Main function of magbot.
  * This function calls itself to stay alive.
  */
-async function run() {
-    log.debug(`Syncing all users...`);
+async function sync() {
+    log.debug(`Going for sync...`);
     try {
         // Get the current magister authcode before syncing users.
         let mAuth = await MagisterAuth();
         // Get all users from DB & run over them.
-        let users = await User.fetchAll();
+        let users = await User.spot({isdisabled: false}).fetchAll();
+        log.info(`Syncing ${users.length} users...`);
         for (let user of users) {
             try {
                 // Log the user in.
@@ -76,8 +80,104 @@ async function run() {
         log.error(`MAJOR SYNC ERROR!`, err);
     }
     const next = scheduleTime();
-    log.debug(`Going for next sync in ${next} seconds...`);
-    setTimeout(run, next);
+    log.info(`Going for next sync in ${next} millis...`);
+    setTimeout(sync, next);
+}
+
+async function purge() {
+    try {
+        // First check sites to make sure we don't accidentally remove users
+        // when magister for instance is down.
+        if (await checkSites()) {
+            throw 'Sites not reachable';
+        }
+        // Get the current magister authcode before purging users.
+        let mAuth = await MagisterAuth();
+        // Get all users from DB & run over them.
+        let users = await User.spot({isdisabled: false}).fetchAll();
+        log.info(`Purging ${users.length} users...`);
+        // Go through all users and purge if necessary.
+        for (let user of users) {
+            try {
+                // Try logging in.
+                await user.login(oAuth, mAuth);
+                // Sleep a bit (0-10 sec) after logging in, to ease magister.
+                await sleep(Math.floor(Math.random() * 10000));
+                // If all tokens etc are valid and we log in,
+                // then we actually get here. Check if there
+                // are calendars left and if so, skip the purge.
+                if (user.hasCalendars()) continue;
+                user.log.warn(`Purging user for not having calendars left!`, {email: user.get('email')});
+            } catch(err) {
+                user.log.warn(`Purging user for `, err);
+            }
+            try {
+                // If possible, warn user they are being placed on non-active.
+                await user.calendar.events.insert({
+                    calendarId: 'primary',
+                    sendUpdates: 'all',
+                    resource: {
+                        summary: 'MAGBOT: Actie vereist! Lees mij!',
+                        description: '<i>Volgens mij klopt er iets niet meer :(</i><br/>' + 
+                                     '<br/>'+
+                                     'Je magister informatie is veranderd of u heeft ' +
+                                     'bewust alle magbot agendas verwijderd.<br/>' +
+                                     '<b>Bij deze wordt uw account op non-actief gezet '+
+                                     'en zal niet automatisch meer werken!</b><br/>' +
+                                     'Heractiveer uw account op ' +
+                                     '<a href="https://magbot.nl/">magbot.nl</a><br/>' +
+                                     '<br/>' +
+                                     'Als u uw account niet meer wilt, onderneem geen ' + 
+                                     'actie en wij zullen u niet meer storen.<br/>' +
+                                     '<br/>' +
+                                     'Stuur bij vragen gerust een mailtje naar ' +
+                                     '<a href="mailto:info@magbot.nl">info@magbot.nl</a>.<br/>',
+                        // Set start and end two days from now at 0900-0930.
+                        start: {
+                            dateTime: new Date(new Date(Date.now() + 1728e5)
+                            .setHours( 9,0,0,0))
+                        },
+                        end: {
+                            dateTime: new Date(new Date(Date.now() + 1728e5)
+                            .setHours(17,0,0,0))
+                        },
+                        location: 'internet',
+                        colorId: 11, // Nice red color
+                        reminders: {
+                            useDefault: false,
+                            overrides: [
+                                { method: 'email', minutes: 1440 },
+                                { method: 'popup', minutes: 1440 },
+                                { method: 'popup', minutes: 5 }
+                            ]
+                        }
+                    }
+                });
+                user.log.info(`Notified user of purge!`);
+            } catch (err) {
+                user.log.error(`Error warning to-be purged user! `, err)
+            }
+            try {
+                user.log.info(`Disabling!`)
+                user.set('isdisabled', true);
+                await user.update();
+            } catch (err) {
+                log.error(`MAJOR PURGE ERROR `, err);
+            }
+        }
+    } catch (err) { log.error('Purging error! ', err); }
+    const next = scheduleTime() * 20;
+    log.info(`Going for next purge in ${next} millis...`);
+    setTimeout(purge, next);
+}
+
+/**
+ * Checks if all involved sites are still
+ * reachable. If not we might get other
+ * errors and don't want to purge users.
+ */
+async function checkSites() {
+    return false;
 }
 
 /**
