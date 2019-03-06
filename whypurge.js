349458aed699a40e4fa6487234e4a5dd25fb459d
7kasper <kasper77077@gmail.com>
2019-03-07 00:48:51 +0100

    Fix donate button and stop sudden deletion of magbot calendar in db...

--- whypurge.js
+++ whypurge.js
@@ -41,31 +41,50 @@ oops();
 
 async function oops() {
     let mAuth = await MagisterAuth();
-    let users = await User.spot({isdisabled: true}).fetchAll();
+    let users = await User.spot({isdisabled: false}).fetchAll();
     log.info(`WHYPURGING ${users.length} users...`);
     // Go through all users and purge if necessary.
     for (let user of users) {
         try {
-            await user.login(oAuth, mAuth);
-            let calsFromGoogle = (await this.calendar.calendarList.list({
+           await user.login(oAuth, mAuth);
+           user.log.info('User could login?'); 
+		   /*
+           let calsFromGoogle = (await user.calendar.calendarList.list({
                 showHidden: true
             })).data.items.reduce((prev, cur) => {
-                prev[cur.id] = cur.defaultReminders;
+                prev[cur.summary] = cur.id;
                 return prev;
             }, {});
             console.dir(calsFromGoogle);
-            break; // Show the user.
-            // let apps = await user.calendarAppointments('primary');
-            // for (let appId in apps) {
-            //     let app = apps[appId];
-            //     if (app.summary.includes('MAGBOT: Actie vereist!')) {
-            //         console.dir(app);
-            //         break;
-            //     }
-            // }
+			if (!('mRegular' in calsFromGoogle) || !('mHomework' in calsFromGoogle) || !('mSpecial' in calsFromGoogle) || !('mOutages' in calsFromGoogle) ) break; // Show the user.
+			user.set('calendars', {
+				regular: calsFromGoogle.mRegular,
+				homework: calsFromGoogle.mHomework,
+				outages: calsFromGoogle.mOutages,
+				special: calsFromGoogle.mSpecial
+			});
+			user.set('isdisabled', false);
+			await user.update();*/
+			
+			let apps = await user.calendarAppointments('primary', false);
+			for (let appId in apps) {
+		         let app = apps[appId];
+			     if (app.summary.includes('MAGBOT: Actie vereist!')) {
+					try {
+						await user.calendar.events.delete({
+							calendarId: 'primary',
+							eventId: appId
+						});
+						user.log.info('removed thingy');
+					} catch(err) {user.log.error('Error removing update!', {err: err.toString()})}
+			     }
+			}
+			
+			user.log.info('FFS Fixed this mofo');
             // break; //Only show me
         } catch (err) {
-            // log.error(err);
+            user.log.error(`user was deleted for: `, {err: err.toString()});
+			//user.destroy();
         }
     }
     log.info('done');
